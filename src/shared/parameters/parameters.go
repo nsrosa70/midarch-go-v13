5e7b2a29f1b0d658c031d63020f43e015a49153b
nsrosa <nsr@cin.ufpe.br>
2018-11-14 11:36:41 -0300

    Initial commit

--- src/shared/parameters/parameters.go
+++ src/shared/parameters/parameters.go
@@ -4,14 +4,14 @@ import "time"
 
 // Dirs
 //const BASE_DIR  = "/go/midarch-go"  // docker
-const BASE_DIR = "/Users/nsr/Dropbox/go/midarch-go-v11"
+const BASE_DIR = "/Users/nsr/Dropbox/go/midarch-go-v12"
 const DIR_PLUGINS = BASE_DIR + "/src/plugins"
 const DIR_CSP = BASE_DIR + "/src/cspspecs"
 const DIR_SOURCE = BASE_DIR
 const DIR_CONF = BASE_DIR + "/src/apps/confs"
 const DIR_GO = "/usr/local/go/bin"
 const DIR_FDR = "/Volumes/Macintosh HD/Applications/FDR4-2.app/Contents/MacOS"
-const DIR_CSPARSER = BASE_DIR+"/src/verificationtools/cspdot/csparser"
+const DIR_CSPARSER = BASE_DIR + "/src/verificationtools/cspdot/csparser"
 const COMPONENTS_PATH = "components"
 const CONNECTORS_PATH = "connectors"
 const NAMINGCLIENTPROXY_PATH = "namingclientproxy"
@@ -49,14 +49,15 @@ const PLUGIN_BASE_NAME = "fibonacciinvoker"
 const GRAPH_SIZE = 30
 
 const MAX_NUMBER_OF_ACTIVE_CONSUMERS = 10
-var IS_EVOLUTIVE  = false
+
+var IS_EVOLUTIVE = false
 var IS_CORRECTIVE = false
-var IS_PROACTIVE  = false
+var IS_PROACTIVE = false
 
 var MONITOR_TIME time.Duration   // seconds
 var INJECTION_TIME time.Duration // seconds
 var REQUEST_TIME time.Duration   // milliseconds
-var STRATEGY = 0             // 1 - no change 2 - change once 3 - change same plugin 4 - alternate plugins
+var STRATEGY = 0                 // 1 - no change 2 - change once 3 - change same plugin 4 - alternate plugins
 var SAMPLE_SIZE = 0
 var NAMING_HOST = ""
 var QUEUEING_HOST = ""
